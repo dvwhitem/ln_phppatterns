70ac1fb39b100832fcf3ef43cbb27958ea5c453a
dvwhitem <dvwhitem@gmail.com>
2012-07-10 23:15:13 +0300

    polymorphism

---
+++ creational/factory_method/commsmanager.php
@@ -0,0 +1,8 @@
+<?php
+    
+    abstract class CommsManager {
+    
+        abstract function getHeaderText();
+        abstract function getApptEncoder();
+        abstract function getFooterText();
+    }
